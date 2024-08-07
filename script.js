6d4c55080444a0701b6b7ca7ab26cb6e34fc51dc
KasiaH <kah.gda@wp.pl>
2024-08-07 14:40:20 +0200

    added answer at answerArr

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ const input = document.querySelector('input');
 const error = document.querySelector('.error');
 const answer = document.querySelector('.answer')
 
-const answersArr = ['Tak!', 'Nie!', 'Może...', 'Cięzko powiedzeć...', 'Nie chcesz znać odpowiedzi na to pytanie...;/'];
+const answersArr = ['Tak!', 'Nie!', 'Może...', 'Cięzko powiedzeć...', 'Nie chcesz znać odpowiedzi na to pytanie...', 'Odpowiem innym razem'];
 
 
 const shakeBall = () => {
-    
     ball.classList.add('shake-animation');
 setTimeout(checkInput, 1000)
 }
-
-
 
 const checkInput = () => {
     if (input.value !== '' && input.value.slice(-1) === '?') {
@@ -30,14 +27,10 @@ const checkInput = () => {
     }
 }
 
-
 const generateAnswer = () => {
     const number = Math.floor(Math.random() * 5);
     answer.innerHTML = `<span>Odpowiedź:</span>  ${answersArr[number]}`
 }
 
-    
-    
-   
-    ball.addEventListener('click', shakeBall);
+ ball.addEventListener('click', shakeBall);
 
